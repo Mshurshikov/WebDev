3a7ddbe04adda8df7ab803675ee21f719d033901
Mikhail Shurshikov <m.shurshikov@gmail.com>
2020-11-18 21:51:44 +0100

    added styles for tasks

--- toDoApp/scripts.js
+++ toDoApp/scripts.js
@@ -1,20 +1,23 @@
 function addTaskToList() {
 
-	let newTask = document.createElement('li');
-
 	let taskList = document.getElementById('taskList');
+	
+	let newTask = document.createElement('li');
 	newTask = taskList.appendChild(newTask);
 	newTask.innerText = document.getElementById('newTask').value;
+	newTask.className = 'task';
 
 	let btnDone = document.createElement('input');
 	btnDone.type = 'button';
 	btnDone.value = 'Done';
+	btnDone.className = 'task';
 	btnDone.addEventListener('click', markTaskAsDone, false); 
 	newTask.appendChild(btnDone);
 
 	let btnDel = document.createElement('input');
 	btnDel.type = 'button';
 	btnDel.value = 'X';
+	btnDel.className ='task';
 	btnDel.addEventListener('click', deleteTask, false);
 	newTask.appendChild(btnDel);
 
