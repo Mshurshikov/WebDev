d1d4441d6ad48b399706c6be705cb99110cec242
Mikhail Shurshikov <m.shurshikov@gmail.com>
2020-11-18 21:22:14 +0100

    updated scripts.js
    added clearList() markTaskAsDone() and deleteTask()

--- toDoApp/scripts.js
+++ toDoApp/scripts.js
@@ -5,8 +5,33 @@ function addTaskToList() {
 	let taskList = document.getElementById('taskList');
 	newTask = taskList.appendChild(newTask);
 	newTask.innerText = document.getElementById('newTask').value;
+
+	let btnDone = document.createElement('input');
+	btnDone.type = 'button';
+	btnDone.value = 'Done';
+	btnDone.addEventListener('click', markTaskAsDone, false); 
+	newTask.appendChild(btnDone);
+
+	let btnDel = document.createElement('input');
+	btnDel.type = 'button';
+	btnDel.value = 'X';
+	btnDel.addEventListener('click', deleteTask, false);
+	newTask.appendChild(btnDel);
+
+	document.getElementById('newTask').value = '';	
 }
 
 function clearList() {
-	alert('Cleared');
+	let taskList = document.getElementById('taskList');
+	while (taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
+	}
+}
+
+function markTaskAsDone() {
+	alert('Done');
+}
+
+function deleteTask() {
+	alert('Delete');
 }
