6a468c69fca8342fa764dc1d5a61a8c1b82a7e54
Mikhail Shurshikov <m.shurshikov@gmail.com>
2020-11-29 19:01:31 +0100

    updated battleship.js

--- battleship/scripts/battleship.js
+++ battleship/scripts/battleship.js
@@ -80,7 +80,7 @@ var model = {
 	}
 }
 
-/* test model */
+/* test model
 model.fire("53"); 		//MISS
 
 model.fire("06");		//HIT
@@ -94,25 +94,90 @@ model.fire("44");
 model.fire("12");		//HIT
 model.fire("11");
 model.fire("10");
+*/
 
 var controller = {
 	guesses: 0,
 
 	processGuess: function(guess) {
-		
+		var location = this.parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sank all my ships in " + this.guesses + " guesses");
+			}
+		}
+	},
+
+	parseGuess: function(guess) {
+		var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+		if (guess === null || guess.length !==2) {
+			alert("Please enter a letter and a number on the board.");
+		} else {
+			firstChar = guess.charAt(0);
+			var row = alphabet.indexOf(firstChar);
+			var column = guess.charAt(1);
+
+			if (isNaN(row) || isNaN(column)) {
+				alert("That isn't on the board");
+			} else if (row < 0 || row >= model.boardSize ||
+							column < 0 || column >= model.boardSize ) {
+				alert("That is off the board");
+			} else {
+				return row + column;
+			}
+		}
+		return null;
 	}
 }
 
+/* test controller 
+console.log(controller.parseGuess("A0"));		// 00
+console.log(controller.parseGuess("B6"));		// 16
+console.log(controller.parseGuess("G3")); 		// 63
+console.log(controller.parseGuess("H0"));		// null
+console.log(controller.parseGuess("A7"));		// null
 
 
+controller.processGuess("A0");
 
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
 
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
 
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
+*/
 
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	
+	var guessInput = document.getElementById("guessInput");
+	guessInput.onkeypress = handleKeyPress;
+}
 
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = "";
+}
 
+function handleKeyPress(e) {
+	var fireButton = document.getElementById("fireButton");
+	if (e.keyCode === 13) {
+		fireButton.click();
+		return false;
+	}
+}
 
-
-
-
+window.onload = init;
 
